c2c6470193fa3ff5babb98f5a69f1038d25ad815
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-10-27 15:41:35 -0700

    gather data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 def get_employee_todo_progress(employee_id):
-
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
     todos_url = f'{base_url}/todos?userId={employee_id}'
@@ -15,8 +14,7 @@ def get_employee_todo_progress(employee_id):
         user_data = user_response.json()
         todos_data = todos_response.json()
 
-        if user_response.status_code != 200 or 
-        todos_response.status_code != 200:
+        if user_response.status_code != 200 or todos_response.status_code != 200:
             print("User or TODO data not found")
             return
 
@@ -37,3 +35,4 @@ if __name__ == '__main__':
     else:
         employee_id = sys.argv[1]
         get_employee_todo_progress(employee_id)
+
